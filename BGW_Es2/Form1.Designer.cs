d6d39a93aa9039eed2c311e10a41bd347569f2e3
BolziFilippo <filippo.bolzi.studenti@isii.it>
2020-11-12 17:45:25 +0100

    Aggiungere i file di progetto.

---
+++ BGW_Es2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace BGW_Es2
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_A2 = new System.Windows.Forms.Button();
+            this.btn_S2 = new System.Windows.Forms.Button();
+            this.txt_box1 = new System.Windows.Forms.TextBox();
+            this.btn_A100 = new System.Windows.Forms.Button();
+            this.btn_S100 = new System.Windows.Forms.Button();
+            this.txt_box2 = new System.Windows.Forms.TextBox();
+            this.bgw3 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btn_A2
+            // 
+            this.btn_A2.Location = new System.Drawing.Point(12, 34);
+            this.btn_A2.Name = "btn_A2";
+            this.btn_A2.Size = new System.Drawing.Size(76, 23);
+            this.btn_A2.TabIndex = 0;
+            this.btn_A2.Text = "Avvio 2";
+            this.btn_A2.UseVisualStyleBackColor = true;
+            this.btn_A2.Click += new System.EventHandler(this.btn_A2_Click);
+            // 
+            // btn_S2
+            // 
+            this.btn_S2.Location = new System.Drawing.Point(12, 73);
+            this.btn_S2.Name = "btn_S2";
+            this.btn_S2.Size = new System.Drawing.Size(76, 23);
+            this.btn_S2.TabIndex = 1;
+            this.btn_S2.Text = "Stop 2";
+            this.btn_S2.UseVisualStyleBackColor = true;
+            this.btn_S2.Click += new System.EventHandler(this.btn_S2_Click);
+            // 
+            // txt_box1
+            // 
+            this.txt_box1.Location = new System.Drawing.Point(110, 24);
+            this.txt_box1.Multiline = true;
+            this.txt_box1.Name = "txt_box1";
+            this.txt_box1.Size = new System.Drawing.Size(100, 340);
+            this.txt_box1.TabIndex = 2;
+            // 
+            // btn_A100
+            // 
+            this.btn_A100.Location = new System.Drawing.Point(376, 34);
+            this.btn_A100.Name = "btn_A100";
+            this.btn_A100.Size = new System.Drawing.Size(76, 23);
+            this.btn_A100.TabIndex = 3;
+            this.btn_A100.Text = "Avvio 100";
+            this.btn_A100.UseVisualStyleBackColor = true;
+            this.btn_A100.Click += new System.EventHandler(this.btn_A100_Click);
+            // 
+            // btn_S100
+            // 
+            this.btn_S100.Location = new System.Drawing.Point(377, 73);
+            this.btn_S100.Name = "btn_S100";
+            this.btn_S100.Size = new System.Drawing.Size(75, 23);
+            this.btn_S100.TabIndex = 4;
+            this.btn_S100.Text = "Stop 100";
+            this.btn_S100.UseVisualStyleBackColor = true;
+            this.btn_S100.Click += new System.EventHandler(this.btn_S100_Click);
+            // 
+            // txt_box2
+            // 
+            this.txt_box2.Location = new System.Drawing.Point(506, 24);
+            this.txt_box2.Multiline = true;
+            this.txt_box2.Name = "txt_box2";
+            this.txt_box2.Size = new System.Drawing.Size(100, 340);
+            this.txt_box2.TabIndex = 5;
+            // 
+            // bgw1
+            // 
+            this.bgw3.WorkerReportsProgress = true;
+            this.bgw3.WorkerSupportsCancellation = true;
+            this.bgw3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw1_DoWork);
+            this.bgw3.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw1_ProgressChanged);
+            this.bgw3.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw1_RunWorkerCompleted);
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_box2);
+            this.Controls.Add(this.btn_S100);
+            this.Controls.Add(this.btn_A100);
+            this.Controls.Add(this.txt_box1);
+            this.Controls.Add(this.btn_S2);
+            this.Controls.Add(this.btn_A2);
+            this.Name = "frm_Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_A2;
+        private System.Windows.Forms.Button btn_S2;
+        private System.Windows.Forms.TextBox txt_box1;
+        private System.Windows.Forms.Button btn_A100;
+        private System.Windows.Forms.Button btn_S100;
+        private System.Windows.Forms.TextBox txt_box2;
+        private System.ComponentModel.BackgroundWorker bgw;
+    }
+}
+
---
+++ BGW_Es2/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGW_Es2
+{
+    public partial class frm_Main : Form
+    {
+        BackgroundWorker bgw2, bgw3;
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_A2_Click(object sender, EventArgs e)
+        {
+            bgw3 = new BackgroundWorker();
+            bgw3.WorkerReportsProgress = true;
+            bgw3.WorkerSupportsCancellation = true;
+            bgw3.DoWork += bgw1_DoWork;
+            bgw3.ProgressChanged += bgw1_ProgressChanged;
+            bgw3.RunWorkerCompleted += bgw1_RunWorkerCompleted;
+
+            bgw2 = new BackgroundWorker();
+            bgw2.WorkerReportsProgress = true;
+            bgw2.WorkerSupportsCancellation = true;
+            bgw2.DoWork += bgw1_DoWork;
+            bgw2.ProgressChanged += bgw1_ProgressChanged;
+            bgw2.RunWorkerCompleted += bgw1_RunWorkerCompleted;
+
+            bgw3.RunWorkerAsync(txt_box1);
+            bgw2.RunWorkerAsync(txt_box2);
+
+
+
+
+
+        }
+
+        private void bgw1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+            TextBox tb = e.Argument as TextBox;
+            for (int i = 0; i < 10 ; i++)
+            {
+                Thread.Sleep(100);
+                bgw.ReportProgress(i, tb);
+                if(bgw.CancellationPending)
+                {
+                    e.Result = (false, tb);
+                    return;
+                }
+
+            }
+            e.Result = (true, tb);
+
+        }
+
+        private void bgw1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            TextBox tb = e.UserState as TextBox;
+            tb.Text = e.ProgressPercentage + Environment.NewLine + tb.Text;
+        }
+
+        private void btn_S2_Click(object sender, EventArgs e)
+        {
+            bgw3.CancelAsync();
+            bgw2.CancelAsync();
+
+        }
+
+        private void bgw1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var (completato, tb) = (ValueTuple<bool, TextBox>)e.Result;
+            tb.Text = completato ? "concluso" + Environment.NewLine + tb.Text:
+            "Cancellato" + Environment.NewLine + tb.Text;
+        }
+
+        
+
+        List<BackgroundWorker> bgw_list = new List<BackgroundWorker>();
+
+        private void btn_S100_Click(object sender, EventArgs e)
+        {
+            foreach(var bgw in bgw_list)
+            {
+                bgw.CancelAsync();
+            }
+        }
+
+        private void btn_A100_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                BackgroundWorker bgw1 = new BackgroundWorker();
+                bgw1.WorkerReportsProgress = true;
+                bgw1.WorkerSupportsCancellation = true;
+                bgw1.DoWork += bgw1_DoWork;
+                bgw1.ProgressChanged += bgw1_ProgressChanged;
+                bgw1.RunWorkerCompleted += bgw1_RunWorkerCompleted;
+                bgw_list.Add(bgw1);
+
+            }
+            foreach(var bgw in bgw_list)
+            {
+                bgw.RunWorkerAsync(txt_box2);
+            }
+        }
+    }
+}
